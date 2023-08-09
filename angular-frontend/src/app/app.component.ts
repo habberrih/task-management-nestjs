ed32abdc3e9ac442249e1723946bbd8eb45e533e
Abdullah Habberrih <abdul2013ly@gmail.com>
2023-08-09 14:33:58 +0200

    refactor: add test controller

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Test, TestService } from './services/test.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-frontend';
+
+  valueFromBackend$: Observable<Test> = this.testService.getUserById(1);
+  constructor(private testService: TestService) {}
 }
