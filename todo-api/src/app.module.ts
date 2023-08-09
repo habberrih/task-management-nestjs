63e1bf9b151d3e41859df66388be1a644c76b779
Abdullah Habberrih <abdul2013ly@gmail.com>
2023-08-09 10:50:06 +0200

    feat: add TypeOrmModule

--- todo-api/src/app.module.ts
+++ todo-api/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TodoModule } from './todo/todo.module';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DB_URL,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule,
+    TodoModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
