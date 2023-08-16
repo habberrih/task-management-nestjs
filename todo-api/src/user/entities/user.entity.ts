669b46f230cd4a3108821fd7a4438a59aff280cf
habberrih <abdul2013ly@gmail.com>
2023-08-16 15:30:43 +0200

    feat: add user entity

--- todo-api/src/user/entities/user.entity.ts
+++ todo-api/src/user/entities/user.entity.ts
@@ -1,1 +1,29 @@
-export class User {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BeforeInsert,
+  BeforeUpdate,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ select: false })
+  password: string;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  emailAndUsernameToLowerCase() {
+    this.email = this.email.toLowerCase();
+    this.username = this.username.toLowerCase();
+  }
+}
