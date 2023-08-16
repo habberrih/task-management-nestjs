5fc91497db0a90e74bfa16855d04bca6cf6896d8
habberrih <abdul2013ly@gmail.com>
2023-08-16 15:32:09 +0200

    refactor: add some features to user

--- todo-api/src/user/user.module.ts
+++ todo-api/src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './services/user.service';
 import { UserController } from './controllers/user.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [UserController],
   providers: [UserService],
 })
